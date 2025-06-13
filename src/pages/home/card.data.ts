76c899b581286e517bacd92eb0552e11d4a41ef2
DMProductionn <goluzin.dima008@mail.ru>
2025-06-13 14:58:11 +0500

    test

--- src/pages/home/card.data.ts
+++ src/pages/home/card.data.ts
@@ -3,21 +3,21 @@ export const CARD_DATA = [
     id: 1,
     title: 'Payment Testing',
     desc: 'Verify the functionality and reliability of various payment methods and transactions',
-    img: '/payment.webp',
-    link: '/testpapas/payment-testing'
+    img: '/testpapas/payment.webp',
+    link: '/payment-testing'
   },
   {
     id: 2,
     title: 'UX Testing',
     desc: 'Evaluate the user experience to identify usability issues and enhance user satisfaction',
     img: '/testpapas/wave.webp',
-    link: '/testpapas/ux-testing'
+    link: '/ux-testing'
   },
   {
     id: 3,
     title: 'Custom-Request',
     desc: 'Submit specific testing requests tailored to your unique needs',
     img: '/testpapas/request.webp',
-    link: '/testpapas/custom-request'
+    link: '/custom-request'
   }, 
 ];
